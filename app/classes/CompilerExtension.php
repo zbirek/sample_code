22424bcf9e1e1ba1fa2ff69831cec3b0194452f5
Jiri Jelinek <jelinekvb@gmail.com>
2015-02-16 11:40:55 +0100

    init loader modules

---
+++ app/classes/CompilerExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CompilerExtension
+ *
+ * @author Jiri
+ */
+class CompilerExtension extends Nette\DI\CompilerExtension{
+    //put your code here
+    
+    public function loadConfiguration() {
+        
+    }
+    
+}
+
+
+
